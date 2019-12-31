6c84077d0d8cb2d309fed53b729c3cc98dcb9d34
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-12-31 18:10:27 +0200

    Small CORS fixes

--- client/serve.js
+++ client/serve.js
@@ -3,24 +3,29 @@ const path = require("path");
 
 const app = express();
 
-// Init Middleware
-app.use(express.json({ extended: false }));
-
 // Server static asses in production
-
 // Set static folder
-app.use(express.static("./build"));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:8000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-app.get("*", (req, res) =>
+app.use(express.static("build"));
+
+app.get("/", (req, res) =>
   res.sendFile(path.resolve(__dirname, "build", "index.html"))
 );
 
 // Node connection
 const hostname = "0.0.0.0";
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, hostname, () => {
   console.log(`Listening on ${hostname} 
 Waiting for connections on ${PORT} 
-Server started.`);
+Client started.`);
 });
